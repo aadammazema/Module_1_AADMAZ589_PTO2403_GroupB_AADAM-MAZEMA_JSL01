dda20d45efd408b2dca55e74e908fc2dba15f2e1
Aadam Mazema <maze.aadam@gmail.com>
2024-07-11 18:26:54 +0200

    Update script.js
    Made changes to the JavaScript in order to make an input syntax validator for a pet adoption website. I managed to do this by using regular expression (regex)

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+  let input = document.getElementById("petInput").value;
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+  let result = ""; // Placeholder for validation result
 
-            document.getElementById('result').innerText = result;
+  // Regular expression to match 'pet_' and followed by alphanumeric characters
+  let regex = /^pet_[a-zA-Z0-9]+$/;
+  // Check if input starts with 'pet_' and followed by alphanumeric characters
+  if (regex.test(input)) {
+    result = "Valid Syntax🟢";
+  } else {
+    result = "Invalid Syntax🔴";
+  }
+
+  document.getElementById("result").innerText = result;
 }
-
-
